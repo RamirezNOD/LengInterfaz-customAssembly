e5b53ebefd7830f76c961a779575256397dd0bed
Leonardo Ramirez <42877882+RamirezNOD@users.noreply.github.com>
2020-04-29 21:45:30 -0700

    Create programa 18.asm

---
+++ programa 18.asm
@@ -0,0 +1,112 @@
+; 18- Encontrar la suma de 35 enteros
+
+n1 = 6
+n2 = 2
+n3 = 5
+n4 = 4
+n5 = 7
+n6 = 6
+n7 = 10
+n8 = 3
+n9 = 2
+n10 = 13
+n11 = 11
+n12 = 9
+n13 = 7
+n14 = 9
+n15 = 1
+n16 = 14
+n17 = 3
+n18 = 9
+n19 = 4
+n20 = 2
+n21 = 4
+n22 = 16
+n23 = 9
+n24 = 1
+n25 = 8
+n26 = 1
+n27 = 9
+n28 = 1
+n29 = 15
+n30 = 1
+n31 = 8
+n32 = 1
+n33 = 9
+n34 = 2
+n35 = 4
+
+.begin:	
+	mov Ra, 0
+	mov Rb, n1
+	add Ra
+	mov Rb, n2
+	add Ra
+	mov Rb, n3
+	add Ra
+	mov Rb, n4
+	add Ra
+	mov Rb, n5
+	add Ra
+	mov Rb, n6
+	add Ra
+	mov Rb, n7
+	add Ra
+	mov Rb, n8
+	add Ra
+	mov Rb, n9
+	add Ra
+	mov Rb, n10
+	add Ra
+	mov Rb, n11
+	add Ra
+	mov Rb, n12
+	add Ra
+	mov Rb, n13
+	add Ra
+	mov Rb, n14
+	add Ra
+	mov Rb, n15
+	add Ra
+	mov Rb, n16
+	add Ra
+	mov Rb, n17
+	add Ra
+	mov Rb, n18
+	add Ra
+	mov Rb, n19
+	add Ra
+	mov Rb, n20
+	add Ra
+	mov Rb, n21
+	add Ra
+	mov Rb, n22
+	add Ra
+	mov Rb, n23
+	add Ra
+	mov Rb, n24
+	add Ra
+	mov Rb, n25
+	add Ra
+	mov Rb, n26
+	add Ra
+	mov Rb, n27
+	add Ra
+	mov Rb, n28
+	add Ra
+	mov Rb, n29
+	add Ra
+	mov Rb, n30
+	add Ra
+	mov Rb, n31
+	add Ra
+	mov Rb, n32
+	add Ra
+	mov Rb, n33
+	add Ra
+	mov Rb, n34
+	add Ra
+	mov Rb, n35
+	add Ra	
+	mov Rd, Ra
+	hlt
